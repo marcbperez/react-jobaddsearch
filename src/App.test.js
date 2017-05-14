2cc62c1e1a3a568c409c65123fffd8425b2384a2
marcbperez <marcbperez@users.noreply.github.com>
2017-05-14 07:10:13 +0100

    Fixed token usage in service and its test.

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Service from './Service'
 
+fetch = jest.fn(() => new Promise(resolve => resolve()));
+
 it('renders without crashing', () => {
-  const service = new Service();
+  const service = new Service('', '');
   const div = document.createElement('div');
   ReactDOM.render(<App service={service}/>, div);
 });
